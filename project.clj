c9c80c452f967655666c5dba04cc427a7bf07db5
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-02-16 23:13:29 +0000

    Initial Checkin

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject pitfalls "0.1.0-SNAPSHOT"
+  :description "Examples of Ontology Problems and Strange Reasoning"
+
+  :dependencies [                 
+                 [uk.org.russet/tawny-owl "0.10-SNAPSHOT"]
+                 [org.clojure/clojure "1.4.0"]])
